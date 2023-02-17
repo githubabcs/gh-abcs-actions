09b4f53d962b58965fc85d8bc3a42ddc478a4dd2
Adam Fasoldt <adamvincent.fasoldt@its.ny.gov>
2023-02-17 08:13:22 -0500

    Fix cat facts script

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -1,6 +1,5 @@
 import requests
 import random
-import sys
 
 # Make an HTTP GET request to the cat-fact API
 cat_url = "https://cat-fact.herokuapp.com/facts"
@@ -18,7 +17,7 @@ for fact in r_obj_list:
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
+    return fact_arr[random.randint(0, len(fact_list) + 1)]
 
 random_fact = select_random_fact(fact_list)
 
