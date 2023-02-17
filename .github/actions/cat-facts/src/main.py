16e6bad78ee960a3fca5099f701f3c38a509d2ea
Adam Fasoldt <adamvincent.fasoldt@its.ny.gov>
2023-02-17 08:25:00 -0500

    Christ, python is weirdly explicit (take 2)

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -1,5 +1,6 @@
-import requests
 import random
+
+import requests
 
 # Make an HTTP GET request to the cat-fact API
 cat_url = "https://cat-fact.herokuapp.com/facts"
@@ -14,10 +15,12 @@ fact_list = []
 for fact in r_obj_list:
     fact_list.append(fact["text"])
 
+
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
     return fact_arr[random.randint(0, len(fact_list) + 1)]
+
 
 random_fact = select_random_fact(fact_list)
 
